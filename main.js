37cd34d43ed3be57e6b1d2a2ffdec9955fc647a5
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-09-20 11:29:47 +0200

    test

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const rollButtonPressed = document.querySelector(".roll-button")
 
 let diceArray = ["img/dice6.png", "img/dice5.png", "img/dice4.png", "img/dice3.png", "img/dice2.png", "img/dice1.png"]
 
+//Adding comment
 rollButtonPressed.addEventListener("click", function() {
     let random1 = Math.floor(Math.random() * diceArray.length);
     let random2 = Math.floor(Math.random() * diceArray.length);
